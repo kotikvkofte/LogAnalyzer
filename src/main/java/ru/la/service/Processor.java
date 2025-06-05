83c80a3e58122c7b2e0494af87a93220a6b0db11
Артём Краковцев <blooender@gmail.com>
2025-06-05 20:47:55 +0700

    fix RECIVED operations logic

--- src/main/java/ru/la/service/Processor.java
+++ src/main/java/ru/la/service/Processor.java
@@ -1,6 +1,7 @@
 package ru.la.service;
 
 import ru.la.model.LogEntry;
+import ru.la.model.OperationType;
 import ru.la.parser.LogParser;
 
 import java.util.ArrayList;
@@ -20,11 +21,26 @@ public class Processor {
                 if (!map.containsKey(logEntry.getUser())){
                     map.put(logEntry.getUser(), new ArrayList<>());
                 }
-
+                if(logEntry.getOperationType() == OperationType.TRANSFERRED){
+                    if (!map.containsKey(logEntry.getToUser())){
+                        map.put(logEntry.getToUser(), new ArrayList<>());
+                    }
+                    map.get(logEntry.getToUser()).add(createRecivedEntry(logEntry));
+                }
                 map.get(logEntry.getUser()).add(logEntry);
             }
         }
 
         return map;
+    }
+
+    private LogEntry createRecivedEntry(LogEntry logEntry) {
+        return LogEntry.builder()
+                .dateTime(logEntry.getDateTime())
+                .user(logEntry.getToUser())
+                .operationType(OperationType.RECIVED)
+                .toUser(logEntry.getUser())
+                .amount(logEntry.getAmount())
+                .build();
     }
 }
